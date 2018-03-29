b348215a525fe86f7f2bdbd5af13a47d9390e92b
dmathew93 <denzel.mathew@gmail.com>
2018-03-28 23:36:56 -0400

    adding spring boot actuator to properly shutdown server

--- src/main/java/configuration/ApplicationConfig.java
+++ src/main/java/configuration/ApplicationConfig.java
@@ -4,6 +4,6 @@ package configuration;
  * Created by denzel on 3/26/18.
  */
 public class ApplicationConfig {
-    public static final String SERVER_HOST = "ws://52.26.186.98:8181/chat";
+    public static final String SERVER_HOST = "ws://denzelmathew.com:8181/chat";
     public static final String OUTBOUND_CHANNEL = "/topic/messages";
 }
